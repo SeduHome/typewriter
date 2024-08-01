5d6369662b35557c6368e26fb0f348826aed9e2e
SeduHome <125974063+SeduHome@users.noreply.github.com>
2024-08-01 11:14:14 +0200

    Add files via upload

---
+++ t.bat
@@ -0,0 +1,238 @@
+@echo off
+
+set /a nummer=0
+
+:a
+
+if "%nummer%"=="1" (
+ set ab=a
+)
+if "%nummer%"=="2" (
+ set ab=b
+)
+if "%nummer%"=="3" (
+ set ab=c
+)
+if "%nummer%"=="4" (
+ set ab=d
+)
+if "%nummer%"=="5" (
+ set ab=e
+)
+if "%nummer%"=="6" (
+ set ab=f
+)
+if "%nummer%"=="7" (
+ set ab=g
+)
+if "%nummer%"=="8" (
+ set ab=h
+)
+if "%nummer%"=="9" (
+ set ab=i
+)
+if "%nummer%"=="10" (
+ set ab=j
+)
+if "%nummer%"=="11" (
+ set ab=k
+)
+if "%nummer%"=="12" (
+ set ab=l
+)
+if "%nummer%"=="13" (
+ set ab=m
+)
+if "%nummer%"=="14" (
+ set ab=n
+)
+if "%nummer%"=="15" (
+ set ab=o
+)
+if "%nummer%"=="16" (
+ set ab=p
+)
+if "%nummer%"=="17" (
+ set ab=q
+)
+if "%nummer%"=="18" (
+ set ab=r
+)
+if "%nummer%"=="19" (
+ set ab=s
+)
+if "%nummer%"=="20" (
+ set ab=t
+)
+if "%nummer%"=="21" (
+ set ab=u
+)
+if "%nummer%"=="22" (
+ set ab=v
+)
+if "%nummer%"=="23" (
+ set ab=w
+)
+if "%nummer%"=="24" (
+ set ab=x
+)
+if "%nummer%"=="25" (
+ set ab=y
+)
+if "%nummer%"=="26" (
+ set ab=z
+)
+if "%nummer%"=="27" (
+ set ab=A
+)
+if "%nummer%"=="28" (
+ set ab=B
+)
+if "%nummer%"=="29" (
+ set ab=C
+)
+if "%nummer%"=="30" (
+ set ab=D
+)
+if "%nummer%"=="31" (
+ set ab=E
+)
+if "%nummer%"=="32" (
+ set ab=F
+)
+if "%nummer%"=="33" (
+ set ab=G
+)
+if "%nummer%"=="34" (
+ set ab=H
+)
+if "%nummer%"=="35" (
+ set ab=I
+)
+if "%nummer%"=="36" (
+ set ab=J
+)
+if "%nummer%"=="37" (
+ set ab=K
+)
+if "%nummer%"=="38" (
+ set ab=L
+)
+if "%nummer%"=="39" (
+ set ab=M
+)
+if "%nummer%"=="40" (
+ set ab=N
+)
+if "%nummer%"=="41" (
+ set ab=O
+)
+if "%nummer%"=="42" (
+ set ab=P
+)
+if "%nummer%"=="43" (
+ set ab=Q
+)
+if "%nummer%"=="44" (
+ set ab=R
+)
+if "%nummer%"=="45" (
+ set ab=S
+)
+if "%nummer%"=="46" (
+ set ab=T
+)
+if "%nummer%"=="47" (
+ set ab=U
+)
+if "%nummer%"=="48" (
+ set ab=V
+)
+if "%nummer%"=="49" (
+ set ab=W
+)
+if "%nummer%"=="50" (
+ set ab=X
+)
+if "%nummer%"=="51" (
+ set ab=Y
+)
+if "%nummer%"=="52" (
+ set ab=Z
+)
+if "%nummer%"=="53" (
+ set ab=0
+)
+if "%nummer%"=="54" (
+ set ab=1
+)
+if "%nummer%"=="55" (
+ set ab=2
+)
+if "%nummer%"=="56" (
+ set ab=3
+)
+if "%nummer%"=="57" (
+ set ab=4
+)
+if "%nummer%"=="58" (
+ set ab=5
+)
+if "%nummer%"=="59" (
+ set ab=6
+)
+if "%nummer%"=="60" (
+ set ab=7
+)
+if "%nummer%"=="61" (
+ set ab=8
+)
+if "%nummer%"=="62" (
+ set ab=9
+)
+if "%nummer%"=="63" (
+ set ab=!
+)
+if "%nummer%"=="64" (
+ set ab=.
+)
+if "%nummer%"=="65" (
+ set ab=_
+)
+if "%nummer%"=="66" (
+ set ab=-
+)
+if "%nummer%"=="67" (
+ set ab=#
+)
+if "%nummer%"=="68" (
+ set ab=*
+)
+
+
+
+echo %ab%%ac%
+
+
+
+if "%nummer%"=="52" (
+ set /a nummer1+=1
+)
+
+if "%nummer1%"=="1" (
+ set ac=a
+)
+
+
+
+
+
+
+
+if "%nummer%"=="52" (
+ set /a nummer=0
+)
+
+
+set /a nummer+=1
+goto a
